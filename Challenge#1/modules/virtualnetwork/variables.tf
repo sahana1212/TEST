f2939f287035367e69b7fd2e880c506bcbd31242
sahana1212 <45448315+sahana1212@users.noreply.github.com>
2022-12-18 17:53:58 +0530

    Create variables.tf

---
+++ Challenge#1/modules/virtualnetwork/variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  description = "The name of the resource group we want to use"
+  default     = "RG-VM"
+}
+
+variable "location" {
+  description = "The location/region where we are crrating the resource"
+  default     = "easteurope"
+}
+
+variable "tags" {
+  description = "The tags to associate the resource we are creating"
+  type        = "map"
+}
+
+variable "virtual_network_name" {
+  description = "Name of the vnet to create"
+  default     = "VNET"
+}
+
+variable "address_space" {
+  description = "The address space that is used by the virtual network."
+  default     = "10.0.0.0/16"
+}
+
+variable "dns_servers" {
+  description = "The DNS servers to be used with vNet."
+  default     = ["10.0.0.4", "10.0.0.5"]
+}
