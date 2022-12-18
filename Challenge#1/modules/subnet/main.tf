81ae865146a8cbcc9cda0ad845c12e4a1ed6437c
sahana1212 <45448315+sahana1212@users.noreply.github.com>
2022-12-18 18:49:25 +0530

    Create main.tf

---
+++ Challenge#1/modules/subnet/main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_subnet" "subnet" {
+  count                = "${var.add_endpoint != true ? length(var.subnets) : 0}"
+  resource_group_name  = "${var.resource_group_name}"
+  name                 = "${lookup(var.subnets[count.index], "name", "")}"
+  virtual_network_name = "${var.vnet_name}"
+  address_prefix       = "${lookup(var.subnets[count.index], "prefix", "")}"
+}
